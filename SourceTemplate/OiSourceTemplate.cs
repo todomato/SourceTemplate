0fa0c3a3eb15988ac1db3bd18e02eddb841901da
andy <messboy000@gmail.com>
2020-05-04 23:46:57 +0800

    first commit

---
+++ SourceTemplate/OiSourceTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace SourceTemplate
+{
+    public static class OiSourceTemplate
+    {
+        [SourceTemplate]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ff<T>(this IEnumerable<T> x)
+        {
+            /*$for (int i = 0; i < $expected$; i++)
+            {
+                x;
+            }*/
+        }
+        [SourceTemplate]
+        public static void re(this string x)
+        {
+            //$return x;
+        }
+        [SourceTemplate]
+        public static void r(this string x)
+        {
+            //$return x;
+        }
+    }
+  
+}
